d85ae6280b2590a89e6fb891f1d2438c1b6cf44a
Fabian <fabianrodriguezb27@gmail.com>
2023-04-13 11:45:50 +0200

    agregar paginator

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
 import { NewCategoryComponent } from 'src/app/modules/category/components/new-category/new-category.component';
@@ -22,6 +23,9 @@ export class CategoryComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'name', 'description', 'actions'];
   dataSource = new MatTableDataSource<CategoryElement>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
 
   getCategories(){
 
@@ -49,6 +53,7 @@ export class CategoryComponent implements OnInit {
       });
 
       this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
+      this.dataSource.paginator = this.paginator;
     }
 
   }
